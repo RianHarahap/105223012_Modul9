0be22ba3a0373b5db1083e22ed320841d99fd27c
RianHarahap <rianharahap2303@gmail.com>
2025-05-22 16:13:31 +0700

    Add files via upload

---
+++ LATIHAN/src/BankTransfer.java
@@ -0,0 +1,12 @@
+public class BankTransfer extends PaymentMethod {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("BankTransfer: Processing payment of " + amount + " IDR.");
+    }
+
+    @Override
+    public void processPayment(double amount, String currency) {
+        System.out.println("BankTransfer: Processing payment of " + amount + " in " + currency + ".");
+    }
+}
+
